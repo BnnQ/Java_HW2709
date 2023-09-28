c722b5ddea6f9ac7c85683e0feb79793a3a67f9f
BnnQ <1v25fa94@gmail.com>
2023-09-28 21:31:21 +0300

    Added utility class calculator

---
+++ src/main/java/me/bnnq/utilities/Calculator.java
@@ -0,0 +1,8 @@
+package me.bnnq.utilities;
+
+public class Calculator
+{
+    public static double percentFromNumber(double number, double percent) {
+        return number * percent / 100;
+    }
+}
